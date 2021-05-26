4c2c52d87647d66802c350137c8217bebc79ad4f
coreyo-git <corey.oconnor@live.com.au>
2021-05-26 20:49:04 +1000

    Restructured to suit commands, added update and addcoin commands

--- Discordbot.py
+++ Discordbot.py
@@ -1,33 +1,33 @@
 import os
 import requests
 import random
-
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-# Watches for client message
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+coinList = ['ethereum','bitcoin']
 
-    if message.content.startswith('!'):
-        #Formats message to suit API Request
-        input = str(message.content)
+@bot.command()
+async def addcoin(ctx, coin):
+    coin = coin.lower()
+    r = requests.get('https://api.coingecko.com/api/v3/simple/price?ids='+coin+'&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true').json()
+    await ctx.send(r)
+
+# Gets Coin stats from Coin List
+@bot.command()
+async def update(ctx):
+    for coin in coinList:
+
+        input = str(coin)
         input = input.strip("!")
         input = input.lower()
-
-        if input.__contains__('addcoin'):
-            await message.channel.send("testing add coin")
-            return
 
         #Requests Coin Info
         r = requests.get('https://api.coingecko.com/api/v3/simple/price?ids='+input+'&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true').json()
@@ -40,24 +40,8 @@ async def on_message(message):
         dayChange = dayChange[0:5]
         input = input.capitalize()
 
-        #Displays a message of the day when requesting
-        badMotd = ["Sell ASAP", "HODL", "Funds are safu", "Should have brought Dogecoin...", "Feels bad...", "panik", ]
-        goodMotd = ["Wait for the dump?", "BITCONNNECCCTTT", "Buy up!", "Sub to Pewdiepie"]
-
-        good = random.randrange(len(goodMotd))
-        bad = random.randrange(len(badMotd))
-
-        # Checks if 24 hour change contains - or not
-        if dayChange.__contains__('-'):
-            motd = ["Sell ASAP", "HODL", "Funds are safu", "Should have brought Dogecoin...", "Feels bad...", "panik", "Diamond hands brotha", "Do u kno da wae?", "Crack Open The Crackers bro, you're eating cheap tonight"]
-            bad = random.randrange(len(badMotd))
-            rand = bad
-        else:
-            motd = ["Wait for the dump?", "BITCONNNECCCTTT", "Buy up!", "Sub to Pewdiepie","Can't hurt to sell bro..","Nice One Bruv", "Fuck yeah bro", "Chicken Dinner?", "Up the dog!", "Nice 1 bruva"]
-            good = random.randrange(len(goodMotd))
-            rand = good
-
-        await message.channel.send("> **"+input+"**" + ": **Price:** `$" + usdPirce + "` | **27 Hour Change:** `%" + dayChange + "` | MOTD: `" + motd[rand] + "`")
+        await ctx.channel.send("> **"+input+"**" + ": **Price:** `$" + usdPirce + "` | **24 Hour Change:** `%" + dayChange + "`")
 
 
-client.run(TOKEN)
+
+bot.run(TOKEN)
